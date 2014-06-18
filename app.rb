0f4141046eda3a975b5da9a13ef599f2ebfffc24
Marco Mornati <ilmorna@gmail.com>
2014-06-19 00:01:06 +0200

    Managed file config name using sinatra environments

--- app.rb
+++ app.rb
@@ -1,6 +1,5 @@
 # encoding: utf-8
 require 'sinatra'
-#require 'haml'
 
 require_relative 'helpers/init'
 class RestMCO < Sinatra::Application
@@ -8,13 +7,18 @@ class RestMCO < Sinatra::Application
 	enable :sessions
 
 	configure :production do
+        RestMCOConfig.load_config('/etc/restmco/restmco.cfg')
+        RestMCOConfig.initialize_log
+        RestMCOConfig.restmco_log.info "Running RestMCO using production mode"
 	end
 
 	configure :development do
-
+        config_file_name = ::File.join( ::File.dirname(__FILE__), 'misc/sysconfig/restmco.cfg' )
+        RestMCOConfig.load_config(config_file_name)
+        RestMCOConfig.initialize_log
+        RestMCOConfig.restmco_log.info "Running RestMCO using development mode"
 	end
 
 end
 
-#require_relative 'helpers/init'
 require_relative 'routes/init'
--- helpers/config.rb
+++ helpers/config.rb
@@ -3,31 +3,40 @@ require 'logger'
 
 module RestMCOConfig
 
-    ini=IniFile.load('/etc/restmco/restmco.cfg', :comment => '#')
 
-    #Read Configuration file
-    LOG_FILE = ini['logger']['LOG_FILE']
-    LOG_LEVEL = ini['logger][LOG_LEVEL']
-    
-    #Create log file if does not exists
-    FileUtils.touch LOG_FILE
-    FileUtils.chown('nobody', 'nobody', LOG_FILE)
-    
-    #Create Log file
-    @@restmco_log = Logger.new(LOG_FILE)
-    
-    case LOG_LEVEL
-        when 'DEBUG'
-            restmco_log.level = Logger::DEBUG
-        when 'INFO'
-            restmco_log.level = Logger::INFO
-        when 'WARN'
-            restmco_log.level = Logger::WARN
-        when 'ERROR'
-            restmco_log.level = Logger::ERROR
+    @ini = nil
+
+    def self.load_config(config_file)
+        @ini=IniFile.load(config_file, :comment => '#')
     end
+
+    def self.initialize_log
+        #Read Configuration file
+        log_file = @ini['logger']['LOG_FILE']
+        log_level = @ini['logger][LOG_LEVEL']
+        
+        #Create log file if does not exists
+        FileUtils.touch log_file
+        FileUtils.chown('nobody', 'nobody', log_file)
+        
+        #Create Log file
+        @@restmco_log = Logger.new(log_file)
+        
+        case log_level
+            when 'DEBUG'
+                restmco_log.level = Logger::DEBUG
+            when 'INFO'
+                restmco_log.level = Logger::INFO
+            when 'WARN'
+                restmco_log.level = Logger::WARN
+            when 'ERROR'
+                restmco_log.level = Logger::ERROR
+        end
+    end
+
 
     def self.restmco_log
         @@restmco_log
     end
+
 end
